1c8a021e4b7125d86a2dd826c98c5d82dab0bd7c
Buckton <n_buckton@tss-hr420-dm36.local>
2018-03-21 15:37:54 -0400

    added button functionality

--- js/script.js
+++ js/script.js
@@ -1,3 +1,37 @@
 (() => {
-  console.log()
+  console.log('video stuff fired!');
+
+  const vidPlayer = document.querySelector('video'),
+        playButton = document.querySelectorAll('button')[0],
+        pauseButton = document.querySelectorAll('button')[1];
+        rewindButton = document.querySelectorAll('button')[2];
+
+
+  function volOn() {
+    vidPlayer.muted = false;
+  }
+
+  function volOff(){
+    vidPlayer.muted = true;
+  }
+
+  function rewindVid(){
+    vidPlayer.currentTime = 0;
+  }
+
+  function playVid(){
+    vidPlayer.play();
+  }
+
+  function pauseVid(){
+    vidPlayer.pause();
+  }
+
+  vidPlayer.addEventListener('mouseover', volOn);
+  vidPlayer.addEventListener('mouseout', volOff);
+
+  rewindButton.addEventListener('click', rewindVid);
+  playButton.addEventListener('click', playVid);
+  pauseButton.addEventListener('click', pauseVid);
+
 })();
